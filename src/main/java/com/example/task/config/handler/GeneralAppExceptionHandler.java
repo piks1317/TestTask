ff8417b90ef6af3135e16aedb150a313e51f2fc6
vova <pidvisennyjvolodimir@gmail.com>
2023-04-07 10:15:44 +0300

    [MES-1]: commit4

--- src/main/java/com/example/task/config/handler/GeneralAppExceptionHandler.java
+++ src/main/java/com/example/task/config/handler/GeneralAppExceptionHandler.java
@@ -1,22 +1,41 @@
 package com.example.task.config.handler;
 
 import com.example.task.model.ErrorMessage;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+
+/**
+ * Class
+ */
 @RestControllerAdvice
-public class GeneralAppExceptionHandler extends BaseExceptionHandler {
+public class GeneralAppExceptionHandler extends ResponseEntityExceptionHandler {
 
-  @ExceptionHandler(Exception.class)
+ @ExceptionHandler(Exception.class)
   public ResponseEntity<ErrorMessage> handleUncaughtException(
-      Exception exception){
+      Exception exception,
+      HttpServletRequest request){
 
-    String message = exception.getMessage() == null ? "Internal Error" : exception.getMessage();
+    ResponseStatus responseStatus = exception.getClass().getAnnotation(ResponseStatus.class);
+    HttpStatus status = responseStatus != null ? responseStatus.value() : HttpStatus.INTERNAL_SERVER_ERROR;
+    String message  = status == HttpStatus.INTERNAL_SERVER_ERROR ? "Internal Error" : exception.getMessage();
 
-   final var bodyOfResponse = determineMessage(message, HttpStatus.INTERNAL_SERVER_ERROR);
 
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(bodyOfResponse);
+    ErrorMessage bodyOfResponse = ErrorMessage.builder()
+        .status(status.value())
+        .description(message)
+        .timestamp(LocalDate.now())
+        .url(request.getRequestURL().toString())
+        .build();
+
+    return ResponseEntity.status(status).body(bodyOfResponse);
   }
 }
