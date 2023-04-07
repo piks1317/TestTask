d26830f2f2bc9d0477f1384644f3cc2ade3c269b
vova <pidvisennyjvolodimir@gmail.com>
2023-04-07 10:56:30 +0300

    [MES-1]: commit10

--- src/main/java/com/example/task/common/Class2.java
+++
@@ -1,6 +0,0 @@
-package com.example.task.common;
-
-public class Class2 {
-  private String name;
-  private String surname;
-}
--- src/main/java/com/example/task/common/Class3.java
+++
@@ -1,4 +0,0 @@
-package com.example.task.common;
-
-public class Class3 {
-}
